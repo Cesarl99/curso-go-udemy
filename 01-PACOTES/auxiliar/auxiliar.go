81194c100bc994f4d382b3267504e08013c5b0cc
Cesar Luiz Gomes <cesar.gomes@gruposplice.com.br>
2022-04-04 17:21:22 -0300

    primeiro commit

---
+++ 01-PACOTES/auxiliar/auxiliar.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Escrever() {
+	fmt.Println("Escrevendo do pacote auxiliar")
+}
---
+++ 03-TIPO DE DADOS/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main(){
+	numero := 100000
+	fmt.Println(numero)
+}
